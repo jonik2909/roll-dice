fd31002fc7fc03004f048a1e8948b4b714f69f44
jonik2909 <boronovjonibek@gmail.com>
2023-10-24 12:59:51 +0900

    feat: initialize flutter app and widgets & variables

---
+++ lib/gradient_container.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'package:first_app/styled_text.dart';
+
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.colors, {super.key});
+
+  final List<Color> colors;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+        colors: colors,
+        begin: startAlignment,
+        end: endAlignment,
+      )),
+      child: const Center(
+        child: StyledText("what is it?"),
+      ),
+    );
+  }
+}
